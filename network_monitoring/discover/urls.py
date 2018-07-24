d836ffcd2d3100f0375144d4da92472b5c47bf30
DESKTOP-HM0THT1\buabu <58070076@kmitl.ac.th>
2018-07-24 17:07:30 +0700

    add more ping discovery test in django

---
+++ network_monitoring/discover/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('all/', views.view_all),
+    path('<int:node_id>/', views.view_node),
+    path('<str:oid>/easy/', views.view_node_easy),
+    path('test-thread/', views.test_thread)
+]
