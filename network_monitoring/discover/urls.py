ed65a85e66479af8f3771176a5e9210b07ba8a66
DESKTOP-HM0THT1\buabu <58070076@kmitl.ac.th>
2018-07-29 19:35:02 +0700

    update name of url for html link

--- network_monitoring/discover/urls.py
+++ network_monitoring/discover/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('network/', views.discover_network),
-    path('scan/network/', views.scanning_ip),
+    path('network/', views.discover_network, name='discover-ip'),
+    path('scan/network/', views.scanning_ip, name='scan-ip'),
 ]
