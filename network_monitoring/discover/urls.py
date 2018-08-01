45ca769e67ba5cc6d00fc5f778fbbeb480c7cbb1
DESKTOP-HM0THT1\buabu <58070076@kmitl.ac.th>
2018-08-01 09:01:37 +0700

    update url path for add to DB

--- network_monitoring/discover/urls.py
+++ network_monitoring/discover/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('network/', views.discover_network, name='discover-ip'),
     path('scan/network/', views.scanning_ip, name='scan-ip'),
+    path('add/network/', views.add_database, name='add-ip'),
+    path('add/', views.add_ip, name='add')
 ]
