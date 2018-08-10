29d5598234174c40dd182750ceb4e58c7a668a6c
DESKTOP-HM0THT1\buabu <58070076@kmitl.ac.th>
2018-08-10 08:04:13 +0700

    test model database

--- network_monitoring/nodeprofile/models.py
+++ network_monitoring/nodeprofile/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+class NodeProfile(models.Model):
+    ip = models.GenericIPAddressField()
+    name = models.CharField(max_length=250)
+    description = models.CharField(max_length=255)
